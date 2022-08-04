5e8d9a5f64030b49b5e268c74cde4efdf6183d64
Wild Rivers <jessicamichele11@gmail.com>
2022-08-04 15:27:16 -0600

    pushing for git to be up to date

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
 var apiKey = "4d56ace7";
 var random = "https://owen-wilson-wow-api.herokuapp.com/wows/random";
 var playBtn = document.querySelector("#playBtn");
-var questionText = document.querySelector("#question");
+var questionText = document.querySelector("question");
+var image = document.getElementById("movie-poster");
+var title = document.getElementById("movie-title");
 
 // click button to start game
 playBtn.addEventListener("click", startGame);
 
-// const playBtn = document.getElementById("playBtn");
+/////////////////////////
 playBtn.addEventListener("click", () => {
   playBtn.style.display = "none";
 
@@ -23,19 +25,6 @@ let question = [
     correctAnswer: "",
   },
 ];
-
-/////////////////////DO WE NEED THIS BELOW?///////////////////
-
-// var formSubmitHandler = function (event) {
-//   event.preventDefault();
-//   setLocalStorage();
-//   var userInput = cityInputEl.value.trim();
-//   var coordinates =
-//     "http://api.openweathermap.org/geo/1.0/direct?q=" +
-//     userInput +
-//     "&appid=" +
-//     apiKey;
-// };
 
 // starting the game
 function startGame(event) {
@@ -59,7 +48,16 @@ function startGame(event) {
           return response.json();
         })
         .then(function (data) {
+          var poster = data.poster;
+          var posterImg = `http://www.omdbapi.com/?apikey=4d56ace7&s=${poster}.jpg`;
           console.log(data);
+
+          var icon = document.createElement("img");
+          icon.setAttribute("src", posterImg);
+          image.append(icon);
+
+          var title = document.createElement("h4");
+          title.textContent = `title: ${data.movie}`;
         });
     });
 }
